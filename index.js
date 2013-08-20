5c0b6d28842b68e5b7766ed7a1ce4f943a91ebff
Wanasit Tanakitrungruang <berrysboy@gmail.com>
2013-08-20 19:28:35 +0900

    Major from imap to inbox

--- index.js
+++
@@ -1,5 +0,0 @@
-
-
-exports.GmailConnectionFactory = require('GmailConnectionFactory').GmailConnectionFactory;
-exports.ImapConnectionFactory = require('ImapConnectionFactory').ImapConnectionFactory;
-exports.ImapConnection = require('ImapConnectionFactory').ImapConnection
